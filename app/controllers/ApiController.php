9ec69a1f740983f0ba5685b9c6000fb8c9e92fd7
AlbertBuluma <albert.buluma1@gmail.com>
2020-09-03 17:27:47 +0300

    Add methods for record tables, isolated_organisms, unhls_results and specimen_rejections

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -30,6 +30,116 @@ class ApiController extends \BaseController {
         return Response::json($data);
     }
 
+    // Receive IDs of all warehouse tables
+    public function warehouseIds()
+    {
+        dd(\Illuminate\Support\Facades\Input::all());
+//        $data = \Illuminate\Support\Facades\Input::get('wards');
+//        return Response::json($data, 200);
+
+    }
+
+    /**
+     * Fetch latest 10 records from table.
+     *
+     * @return Response
+     */
+    public static function fetchTableRecords($table)
+    {
+        $records = DB::table($table)->paginate(10);
+        return Response::json($records);
+    }
+
+
+    /**
+     * Display a listing of the fetch Isolated Organisms.
+     *
+     * @return Response
+     */
+    public static function fetchIsolatedOrganisms()
+    {
+
+        $results = DB::table('isolated_organisms')
+            ->leftJoin('drug_susceptibility', function ($join){
+                $join->on('isolated_organisms.id', '=', 'drug_susceptibility.isolated_organism_id');
+            })
+            ->leftJoin('organisms', function($join){
+                $join->on('isolated_organisms.organism_id', '=', 'organisms.id');
+            })
+            ->leftJoin('drug_susceptibility_measures', function($join){
+                $join->on('drug_susceptibility.drug_susceptibility_measure_id', '=', 'drug_susceptibility_measures.id');
+            })
+            ->leftJoin('drugs', function($join){
+                $join->on('drug_susceptibility.drug_id', '=', 'drugs.id');
+            })
+            ->where('isolated_organisms.organism_id', '!=', 'NULL')
+            ->select('isolated_organisms.test_id', 'isolated_organisms.organism_id', 'organisms.name',
+                'drug_susceptibility.zone_diameter', 'drugs.name', 'drugs.description',
+                'drug_susceptibility_measures.symbol', 'drug_susceptibility_measures.interpretation')
+            ->get();
+
+        return Response::json($results, 200);
+    }
+
+
+    /**
+     * Display a listing of the UNHLS Test Results.
+     *
+     * @return Response
+     */
+    public static function fetchUnhlsResults()
+    {
+
+        $results = DB::table('unhls_test_results')
+                    ->leftJoin('measures', function($join){
+                        $join->on('unhls_test_results.measure_id', '=', 'measures.id');
+                    })
+                    ->leftjoin('measure_types', function($join){
+                        $join->on('measures.measure_type_id', '=', 'measure_types.id');
+                    })
+                    ->select('unhls_test_results.id', 'unhls_test_results.measure_id', 'unhls_test_results.result', 'measures.name', 'measure_types.name')
+                    ->get();
+
+        //TODO measure_types.name to select column results
+
+        return Response::json($results, 200);
+    }
+
+
+    /**
+     * Display a listing of the Specimen Rejection warehouse data.
+     *
+     * @return Response
+     */
+    public static function specimenRejections()
+    {
+
+        $results = DB::table('analytic_specimen_rejections')
+                    ->leftJoin('analytic_specimen_rejection_reasons', function ($join){
+                        $join->on('analytic_specimen_rejections.rejection_reason_id', '=', 'analytic_specimen_rejection_reasons.rejection_id');
+                    })
+                    ->leftJoin('rejection_reasons', function($join){
+                        $join->on('rejection_reasons.id', '=', 'analytic_specimen_rejections.rejection_reason_id');
+                    })
+                    ->select('analytic_specimen_rejections.test_id', 'analytic_specimen_rejection_reasons.specimen_id',
+                            'analytic_specimen_rejection_reasons.rejection_id', 'analytic_specimen_rejection_reasons.reason_id',
+                            'rejection_reasons.reason')
+                    ->get();
+
+        return Response::json($results, 200);
+    }
+
+
+    public static function measureRanges()
+    {
+        $results = DB::table('measure_ranges')
+                        ->select('measure_id', 'alphanumeric', 'interpretation')
+                        ->get();
+
+        return Response::json($results, 200);
+
+    }
+
 	/**
 	 * Display a listing of the resource.
 	 *
